e5722cddaec79ccfc6339fb9da7ac9dbad3f81cb
Blackmile <stanley.ezeifeanyi@gmail.com>
2026-03-10 03:39:34 +0100

    added context for authenticating users in localstorage

---
+++ frontend/src/app/context/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import AuthContext  from "./AuthContext";
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const login = async (email, password) => {
+    if (password === "password") {
+      const newUser = { email, name: email.split("@")[0] };
+
+      setUser(newUser);
+      localStorage.setItem("user", JSON.stringify(newUser));
+
+      return true;
+    }
+    return false;
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, login, logout, isAuthenticated: !!user }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import { router } from './routes/router.jsx'
+import { router } from './app/routes/router'
+import { AuthProvider } from './app/context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
