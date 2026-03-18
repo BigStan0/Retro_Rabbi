007be3d55f4e29fb239ff10dff3c161d1e8f4807
Blackmile <stanley.ezeifeanyi@gmail.com>
2026-03-18 01:24:54 +0100

    installed backend structure

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World from express retro rabbi!");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
